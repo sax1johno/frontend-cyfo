1c8fcceeb7f53f7c72be6abb5b844bdb49476bc9
Abdullah Alger <abdullahalger@me.com>
2015-10-10 19:20:05 +0200

    got the styling for the email, the bootcamp message and the benefits done

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			{test: /\.html$/, loader: 'raw'},
 			{test: /\.styl$/, loader: 'style!css!stylus'},
 			{test: /\.css$/, loader: 'style!css'},
-			{test: /\.(png!jpg!jpeg)/, loader: 'file'},
+			{test: /\.(png|jpg|jpeg)$/, loader: 'file'},
 			{test: /\.js$/, loader: 'babel?stage=1', exclude: [/node_modules/, /\.spec\.js/]}
 		]
 	},
