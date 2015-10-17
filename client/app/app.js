7e82d8017698d81bf8174e2d698d5ed6b19d1017
Abdullah Alger <abdullahalger@me.com>
2015-10-17 13:09:32 +0200

    installed angular material

--- client/app/app.js
+++ client/app/app.js
@@ -2,12 +2,14 @@ import 'normalize.css';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import {appDirective} from './app.directive';
+import angularMaterial from 'angular-material';
 
 
 import {home} from './components/home/home';
 
 angular.module('app', [
   uiRouter,
+  angularMaterial,
   home.name
 ])
 .directive('app', appDirective);
--- client/app/components/home/home.js
+++ client/app/components/home/home.js
@@ -1,8 +1,9 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import angularMaterial from 'angular-material';
 import {homeDirective} from './home.directive';
 
-export const home = angular.module('home', [uiRouter])
+export const home = angular.module('home', [uiRouter, angularMaterial])
   .config(function($stateProvider, $urlRouterProvider) {
       $urlRouterProvider.otherwise('/');
 
